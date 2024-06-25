3b2e81a306352a308884cd3e9022b45c1ac07c00
CheeGuang <123812015+CheeGuang@users.noreply.github.com>
2024-06-25 18:50:38 +0800

    Configured Chatbot feature

--- modules/chatbot/chatbot.routes.js
+++ modules/chatbot/chatbot.routes.js
@@ -3,22 +3,12 @@
 const express = require("express");
 
 // ========== Controllers ==========
-// Initialising getTopic
-const getTopic = require("./controllers/getTopic");
-// Initialising getSpecialisedTopic
-const getSpecialisedTopic = require("./controllers/getSpecialisedTopic");
 
 // ========== Set-up ==========
 // Initialising topicRoutes
-const topicRoutes = express.Router();
+const chatbotController = express.Router();
 
 // ========== Routes ==========
-// GET Topic
-topicRoutes.get("/", getTopic);
-// To call this controller: http://localhost:8000/api/topic/
-// GET Specialised Topic
-topicRoutes.get("/specialised", getSpecialisedTopic);
-// To call this controller: http://localhost:8000/api/topic/specialised
 
 // ========== Export ==========
-module.exports = topicRoutes;
+module.exports = chatbotController;
