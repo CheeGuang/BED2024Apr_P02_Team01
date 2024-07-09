33e61b50225e9c91fe7f15b8f2d4db95ca9c932c
CheeGuang <123812015+CheeGuang@users.noreply.github.com>
2024-07-09 14:41:31 +0800

    Rebase to Main

--- modules/doctor/doctor.routes.js
+++ modules/doctor/doctor.routes.js
@@ -32,16 +32,6 @@ doctorRoutes.put(
   "/updateProfession/:id",
   doctorController.updateDocAccountProfession
 );
-doctorRoutes.put("/updateName/:id", doctorController.updateDocAccountName);
-doctorRoutes.put(
-  "/updateContact/:id",
-  doctorController.updateDocAccountContact
-);
-doctorRoutes.put("/updateDOB/:id", doctorController.updateDocAccountDOB);
-doctorRoutes.put(
-  "/updateProfession/:id",
-  doctorController.updateDocAccountProfession
-);
 doctorRoutes.delete("/:id", authorizeUser, doctorController.deleteDoctor); // DELETE for deleting doctors
 // ========== Export ==========
 module.exports = doctorRoutes;
