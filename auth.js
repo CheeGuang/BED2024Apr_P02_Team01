ff061d92b6174f296911f6d29b538ebb77620d0b
Raeanne <raeannezou@gmail.com>
2024-07-15 21:25:17 +0800

    Try 3

--- auth.js
+++ auth.js
@@ -7,40 +7,40 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 // Set up nodemailer with OAuth2
-const createTransporter = async () => {
-  const oauth2Client = new OAuth2(
-    process.env.googleClientId,
-    process.env.googleClientSecret,
-    "https://developers.google.com/oauthplayground" // or your own redirect URI
-  );
+//const createTransporter = async () => {
+// const oauth2Client = new OAuth2(
+//   process.env.googleClientId,
+//   process.env.googleClientSecret,
+//   "https://developers.google.com/oauthplayground" // or your own redirect URI
+// );
 
-  oauth2Client.setCredentials({
-    refresh_token: process.env.REFRESH_TOKEN,
-  });
+// oauth2Client.setCredentials({
+//   refresh_token: process.env.REFRESH_TOKEN,
+// });
 
-  const accessToken = await new Promise((resolve, reject) => {
-    oauth2Client.getAccessToken((err, token) => {
-      if (err) {
-        reject("Failed to create access token");
-      }
-      resolve(token);
-    });
-  });
+//const accessToken = await new Promise((resolve, reject) => {
+//  oauth2Client.getAccessToken((err, token) => {
+//    if (err) {
+//      reject("Failed to create access token");
+//    }
+//    resolve(token);
+//  });
+//});
 
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      type: "OAuth2",
-      user: process.env.EMAIL, // your gmail address
-      clientId: process.env.googleClientId,
-      clientSecret: process.env.googleClientSecret,
-      refreshToken: process.env.REFRESH_TOKEN, // Get the refresh token
-      accessToken,
-    },
-  });
+//const transporter = nodemailer.createTransport({
+//  service: "gmail",
+//  auth: {
+//    type: "OAuth2",
+//    user: process.env.EMAIL, // your gmail address
+//    clientId: process.env.googleClientId,
+//    clientSecret: process.env.googleClientSecret,
+//    refreshToken: process.env.REFRESH_TOKEN, // Get the refresh token
+//    accessToken,
+//  },
+// });
 
-  return transporter;
-};
+// return transporter;
+//};
 
 passport.use(
   new GoogleStrategy(
@@ -48,7 +48,7 @@ passport.use(
       clientID: process.env.googleClientId,
       clientSecret: process.env.googleClientSecret,
       callbackURL: "/auth/google/callback",
-      scope: ["profile", "email", "https://www.googleapis.com/auth/calendar"], // Request access to google calendar API
+      //scope: ["profile", "email", "https://www.googleapis.com/auth/calendar"], // Request access to google calendar API
     },
     async (refreshToken, profile, done) => {
       const { sub, email, given_name, family_name, picture } = profile._json;
@@ -92,35 +92,35 @@ passport.deserializeUser(async (id, done) => {
   try {
     const user = await Patient.getPatientById(id);
     // Ensure that tokens are included
-    const tokens = {
-      accessToken: user.accessToken,
-      refreshToken: user.refreshToken,
-    };
-    done(null, { ...user, ...tokens });
-    //done(null, user);
+    // const tokens = {
+    //   accessToken: user.accessToken,
+    //  refreshToken: user.refreshToken,
+    // };
+    //done(null, { ...user, ...tokens });
+    done(null, user);
   } catch (err) {
     done(err, null);
   }
 });
 
 // Function to get a new access token using the refresh token
-const getNewAccessToken = async (refreshToken) => {
-  const oauth2Client = new google.auth.OAuth2(
-    process.env.googleClientId,
-    process.env.googleClientSecret,
-    "http://localhost:8000/auth/google/callback" // Adjust according to setup
-  );
+//const getNewAccessToken = async (refreshToken) => {
+//  const oauth2Client = new google.auth.OAuth2(
+//    process.env.googleClientId,
+//    process.env.googleClientSecret,
+//    "http://localhost:8000/auth/google/callback" // Adjust according to setup
+//  );
 
-  oauth2Client.setCredentials({ refresh_token: refreshToken });
+//  oauth2Client.setCredentials({ refresh_token: refreshToken });
 
-  try {
-    const tokenResponse = await oauth2Client.getAccessToken();
-    return tokenResponse.token;
-  } catch (err) {
-    console.error("Error generating new access token:", err);
-    throw err;
-  }
-};
+//  try {
+//    const tokenResponse = await oauth2Client.getAccessToken();
+//    return tokenResponse.token;
+//  } catch (err) {
+//    console.error("Error generating new access token:", err);
+//    throw err;
+//  }
+//};
 
 // Additional setup for Google Calendar API
 //const oauth2Client = new google.auth.OAuth2(
