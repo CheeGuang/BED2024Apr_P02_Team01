ea63be55350524c4b56acfbd438898b34deeb166
Raeanne <raeannezou@gmail.com>
2024-07-20 18:45:13 +0800

    Debugging email MC 2

--- public/js/patientViewAppointment.js
+++ public/js/patientViewAppointment.js
@@ -327,6 +327,8 @@ document.addEventListener("DOMContentLoaded", function () {
           .then((response) => {
             if (!response.ok) {
               throw new Error("Network response was not ok");
+            } else {
+              console.log("MC Email sent successfully!");
             }
             //return response.blob();
           })
